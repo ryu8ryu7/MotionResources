c16dcd23fca86d7f66ff69226c050646c0bb4f58
DESKTOP-F632G4Q\ooi11 <ryu8ryu7@yahoo.co.jp>
2022-02-17 01:11:46 +0900

    追加

--- AssetPackages/Malbers Animations/Common/Cinemachine/CameraInputMapper.cs
+++ AssetPackages/Malbers Animations/Common/Cinemachine/CameraInputMapper.cs
@@ -1,32 +1,32 @@
-﻿using Cinemachine;
-using UnityEngine;
+﻿//using Cinemachine;
+//using UnityEngine;
 
-/// <summary>
-/// Thanks Tahir Alir!
-/// </summary>
-namespace MalbersAnimations
-{
-    public class CameraInputMapper : MonoBehaviour
-    {
-        public string TouchXInputMapTo = "Mouse X";
-        public string TouchYInputMapTo = "Mouse Y";
+///// <summary>
+///// Thanks Tahir Alir!
+///// </summary>
+//namespace MalbersAnimations
+//{
+//    public class CameraInputMapper : MonoBehaviour
+//    {
+//        public string TouchXInputMapTo = "Mouse X";
+//        public string TouchYInputMapTo = "Mouse Y";
 
-        Vector2 delta;
-        void Start()
-        {
-            CinemachineCore.GetInputAxis = GetInputAxis;
-        }
+//        Vector2 delta;
+//        void Start()
+//        {
+//            CinemachineCore.GetInputAxis = GetInputAxis;
+//        }
 
-        private float GetInputAxis(string axisName)
-        {
-            if (string.Equals(axisName, TouchXInputMapTo))
-                return delta.x;
-            if (string.Equals(axisName, TouchYInputMapTo))
-                return delta.y;
+//        private float GetInputAxis(string axisName)
+//        {
+//            if (string.Equals(axisName, TouchXInputMapTo))
+//                return delta.x;
+//            if (string.Equals(axisName, TouchYInputMapTo))
+//                return delta.y;
 
-            return 0;
-        }
+//            return 0;
+//        }
 
-        public void CameraInput(Vector2 value) => delta = value;
-    }
-}
+//        public void CameraInput(Vector2 value) => delta = value;
+//    }
+//}
